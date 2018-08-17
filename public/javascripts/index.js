d6bd3dbc623790095e022a74c9e871556181595c
Kevin Reynolds <kreynolds@brightcove.com>
2018-08-17 11:23:48 -0400

    serverside implementation of getting access token

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,100 +1,125 @@
-var BCLS = ( function (window, document) {
-  var clientId   = document.getElementById('clientId'),
-    clientSecret = document.getElementById('clientSecret'),
-    submitButton = document.getElementById('submitButton'),
-    accessToken  = document.getElementById('accessToken'),
-    apiResponse  = document.getElementById('apiResponse'),
-    options      = {},
-    proxyURL     = 'http://cs1.brightcodes.net/jcefalo/cms_tool/proxy.php',
-    access_token;
+var BCLS = (function(window, document) {
+	var clientId = document.getElementById('clientId'),
+		clientSecret = document.getElementById('clientSecret'),
+		// submitButton = document.getElementById('submitButton'),
+		accessToken = document.getElementById('accessToken'),
+		accessTokenForm = document.getElementById('accessTokenForm'),
+		accessTokenButton = document.getElementById('accessTokenButton'),
+		apiResponse = document.getElementById('apiResponse'),
+		options = {},
+		proxyURL = 'http://cs1.brightcodes.net/jcefalo/cms_tool/proxy.php',
+		access_token;
 
-// event handlers
-submitButton.addEventListener('click', function() {
-  var responseParsed;
-  if (isDefined(clientId.value) && isDefined(clientSecret.value)) {
-    options.client_id     = clientId.value;
-    options.client_secret = clientSecret.value;
-    makeRequest(options, function(response) {
-      if (isJson(response)) {
-        responseParsed          = JSON.parse(response);
-        access_token            = responseParsed.access_token;
-        accessToken.textContent = access_token;
-        // apiResponse.textContent = JSON.stringify(responseParsed, null, '  ');
-      } else {
-        // didn't get JSON back, just dump responseRaw
-        apiResponse.textContent = response;
-      }
-    });
-  } else {
-    alert('Client ID & Secret Are Required');
-  }
-});
+	accessTokenButton.addEventListener('click', function(evt) {
+		evt.preventDefault();
+		let body = {
+			clientId: accessTokenForm.elements.clientId.value,
+			clientSecret: accessTokenForm.elements.clientSecret.value
+		};
+		console.log(body);
+		fetch('/getAccessToken', {
+			method: 'POST',
+			body: JSON.stringify(body),
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(response => {
+				return response.json();
+			})
+			.then(function(data) {
+				// `data` is the parsed version of the JSON returned from the above endpoint.
+				console.log(data);
+				accessToken.innerText = data;
+			});
+	});
+	// event handlers
+	// submitButton.addEventListener('click', function() {
+	//   var responseParsed;
+	//   if (isDefined(clientId.value) && isDefined(clientSecret.value)) {
+	//     options.client_id     = clientId.value;
+	//     options.client_secret = clientSecret.value;
+	//     makeRequest(options, function(response) {
+	//       if (isJson(response)) {
+	//         responseParsed          = JSON.parse(response);
+	//         access_token            = responseParsed.access_token;
+	//         accessToken.textContent = access_token;
+	//         // apiResponse.textContent = JSON.stringify(responseParsed, null, '  ');
+	//       } else {
+	//         // didn't get JSON back, just dump responseRaw
+	//         apiResponse.textContent = response;
+	//       }
+	//     });
+	//   } else {
+	//     alert('Client ID & Secret Are Required');
+	//   }
+	// });
 
-accessToken.addEventListener('click', function() {
-  this.select();
-});
+	// accessToken.addEventListener('click', function() {
+	//   this.select();
+	// });
 
-/**
- * tests for all the ways a variable might be undefined or not have a value
- * @param {*} x the variable to test
- * @return {Boolean} true if variable is defined and has a value
- */
-function isDefined(x) {
-    if ( x === '' || x === null || x === undefined) {
-        return false;
-    }
-    return true;
-}
+	/**
+	 * tests for all the ways a variable might be undefined or not have a value
+	 * @param {*} x the variable to test
+	 * @return {Boolean} true if variable is defined and has a value
+	 */
+	function isDefined(x) {
+		if (x === '' || x === null || x === undefined) {
+			return false;
+		}
+		return true;
+	}
 
-/*
+	/*
  * tests to see if a string is json
  * @param {String} str string to test
  * @return {Boolean}
  */
-function isJson(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
-}
+	function isJson(str) {
+		try {
+			JSON.parse(str);
+		} catch (e) {
+			return false;
+		}
+		return true;
+	}
 
-/**
- * send API request to the proxy
- * @param  {Object} options options for the request
- * @param  {String} requestID the type of request = id of the button
- * @param  {Function} [callback] callback function
- */
-function makeRequest(options, callback) {
-  var httpRequest = new XMLHttpRequest(),
-    responseRaw,
-    requestParams,
-    // response handler
-    getResponse = function() {
-      try {
-        if (httpRequest.readyState === 4) {
-          if (httpRequest.status >= 200 && httpRequest.status < 300) {
-            // check for completion
-            responseRaw = httpRequest.responseText;
-            callback(responseRaw);
-          }
-        }
-      } catch (e) {
-        alert('Caught Exception: ' + e);
-      }
-    };
-  // set up request data
-  requestParams = 'client_id=' + options.client_id + '&client_secret=' + options.client_secret;
+	/**
+	 * send API request to the proxy
+	 * @param  {Object} options options for the request
+	 * @param  {String} requestID the type of request = id of the button
+	 * @param  {Function} [callback] callback function
+	 */
+	function makeRequest(options, callback) {
+		var httpRequest = new XMLHttpRequest(),
+			responseRaw,
+			requestParams,
+			// response handler
+			getResponse = function() {
+				try {
+					if (httpRequest.readyState === 4) {
+						if (httpRequest.status >= 200 && httpRequest.status < 300) {
+							// check for completion
+							responseRaw = httpRequest.responseText;
+							callback(responseRaw);
+						}
+					}
+				} catch (e) {
+					alert('Caught Exception: ' + e);
+				}
+			};
+		// set up request data
+		requestParams = 'client_id=' + options.client_id + '&client_secret=' + options.client_secret;
 
-  // set response handler
-  httpRequest.onreadystatechange = getResponse;
-  // open the request
-  httpRequest.open('POST', proxyURL);
-  // set headers
-  httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  // open and send request
-  httpRequest.send(requestParams);
-}
-
+		// set response handler
+		httpRequest.onreadystatechange = getResponse;
+		// open the request
+		httpRequest.open('POST', proxyURL);
+		// set headers
+		httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		// open and send request
+		httpRequest.send(requestParams);
+	}
 })(window, document);
