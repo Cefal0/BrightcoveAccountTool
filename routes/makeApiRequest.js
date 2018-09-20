970bcdc47b9eb053374dbecac91a71155cc53bc6
Joseph Cefalo <cefalo.joe@gmail.com>
2018-09-20 13:06:36 -0400

    Adjust for Playback API

--- routes/makeApiRequest.js
+++ routes/makeApiRequest.js
@@ -3,10 +3,32 @@ const router = express.Router();
 const request = require("request-promise-native");
 /* post requets */
 router.post("/", function(req, res, next) {
-  console.log(req.body);
+  // console.log(req.body);
   const bearerToken = req.body.bearer_id;
-  if (req.body.requestType === "GET") {
-    console.log("Method is GET");
+  const policyKey = req.body.policy_key;
+  const apiBase = req.body.apiBase;
+  if(req.body.apiBase === "https://edge.api.brightcove.com/playback/v1") {
+    // console.log("Using Policy Key");
+    const makeApiRequest = request ({
+      method: req.body.requestType,
+      url: `${req.body.apiRequest}`,
+      headers: {
+        Authorization: `BCOV-Policy ${policyKey}`,
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      // console.log(response);
+      const apiResponse = JSON.parse(response);
+      return res.json(apiResponse);
+    })
+    .catch(err => {
+      // console.log(err);
+      return res.json(err);
+    });
+  }
+  else if(req.body.requestType === "GET") {
+    // console.log("Method is GET");
     const makeApiRequest = request({
       method: req.body.requestType,
       url: `${req.body.apiRequest}`,
@@ -17,12 +39,12 @@ router.post("/", function(req, res, next) {
     })
       .then(response => {
         // const apiResponse = JSON.parse(response).api_response;
-        console.log(response);
+        // console.log(response);
         const apiResponse = response;
         return res.json(apiResponse);
       })
       .catch(err => {
-        console.log(err);
+        // console.log(err);
         return res.json(err);
       });
   } else {
@@ -37,7 +59,7 @@ router.post("/", function(req, res, next) {
     })
       .then(response => {
         const apiResponse = JSON.parse(response);
-        console.log("Response from Request: " + apiResponse);
+        // console.log("Response from Request: " + apiResponse);
         return res.json(apiResponse);
       })
       .catch(err => {
