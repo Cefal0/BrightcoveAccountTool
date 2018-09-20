9d2bffa08bbfb75a3b8687f48fce188c3ddaac41
Kevin Reynolds <kreynolds@brightcove.com>
2018-09-20 13:28:02 -0400

    set hostname to localhost on master branch

--- public/javascripts/requests.js
+++ public/javascripts/requests.js
@@ -26,7 +26,7 @@ var makeApiRequest = (function(window, document) {
     // console.log("Button is Clicked");
     if (isDefined(bearer_id.value) && isDefined(apiRequest.value) || isDefined(policy_key.value) && isDefined(apiRequest.value)) {
       // console.log(body); // checking purposes
-      fetch("http://xicsprojects:4000/makeApiRequest", {
+      fetch("/makeApiRequest", {
         method: "POST",
         body: JSON.stringify(body),
         headers: {
